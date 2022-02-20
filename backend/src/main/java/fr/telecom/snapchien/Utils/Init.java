c88fa2808bf7ed046a7b7c0e00d0dc42f0f4dde8
Jaaaaj <ruben.felicianotse@gmail.com>
2022-02-20 13:07:30 +0100

    Fix command line runner

--- backend/src/main/java/fr/telecom/snapchien/Utils/Init.java
+++ backend/src/main/java/fr/telecom/snapchien/Utils/Init.java
@@ -11,6 +11,13 @@ public class Init implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// On cree le dossier photos s'il n'existe pas
+		File directory = new File("./photos");
+
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+
 		// On supprime le contenu du dossier photo puisqu'on reset la bdd a chaque fois
 		FileUtils.cleanDirectory(new File("./photos"));
 		System.out.println("Initialisation terminee");
